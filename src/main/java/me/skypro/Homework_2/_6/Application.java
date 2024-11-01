d418347acaf831300574c022dcebde4421e9f39b
Leysen95 <mullayanova95@bk.ru>
2024-11-01 20:22:26 +0500

    initial commit

---
+++ src/main/java/me/skypro/Homework_2/_6/Application.java
@@ -0,0 +1,13 @@
+package me.skypro.Homework_2._6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
